e52b7ed2b21446699dc956c08e55c3c4e030c255
Nathan Vieira <nathanvmag@gmail.com>
2016-10-07 12:25:12 -0300

    FirstCommit

---
+++ PokemonGo3D/Assets/Die.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Die : MonoBehaviour 
+{
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.tag == "Player") 
+		{
+			Debug.Log("colliding");
+			Application.LoadLevel("Game");
+		}
+	}
+}
