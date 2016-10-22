a412e0698dca79b86eb67c72823b8dcafa62af32
Ronald Albert <ronaldalbert1609@gmail.com>
2016-10-22 19:32:35 -0200

    AddedNetworkToTheGame
    Player can now connect to the game using a network

---
+++ PokemonGo3D/Assets/Scripts/Network/NetworkPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkPlayer : Photon.MonoBehaviour
+{
+    [SerializeField]
+    private GameObject myCamera;
+    void Start()
+    {
+        if (photonView.isMine)
+        {
+            myCamera.SetActive(true);
+            this.GetComponent<PlayerScript>().enabled = true;
+        }
+        else
+        {
+            myCamera.SetActive(false);
+            this.GetComponent<PlayerScript>().enabled = false;
+        }
+    }
+}
